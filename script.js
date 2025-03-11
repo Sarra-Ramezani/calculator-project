151a1aad5a74e3551c0b092d0aabf330fbf4efe2
Sarra-Ramezani <160700585+Sarra-Ramezani@users.noreply.github.com>
2025-03-11 21:13:31 +0330

    1. fix clear last digit 2. set limitation decimal places 3. fix negative and % problem of replacing first matched operand 4. add divide by zero alert

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let btnValue = 0; //stores target button value
 let currentOperation = ""; //stores the whole operation step by step
 let result = 0; // stores the result of current operation
+let lastOperand;
 //1. listen for click on parent, instead of each button child
 //2. store value of the clicked button in btnValue
 document
@@ -30,16 +31,28 @@ document
       currentOperation += btnValue.toString();
       displayResult(currentOperation);
     } else if (btnValue === "equals") {
-      displayPreviousOperation(currentOperation);
-      //convert the currentOperation to mathematical operation then store the result
-      result = new Function(`return ${currentOperation}`)();
-      //to continue the opertaion, we set result to currentOperation
-      currentOperation = result;
-      displayResult(result);
+      //check if there is any division by zero
+      if (checkZeroDivision(currentOperation))
+        alert("You can't divide by zero! Try another operation.");
+      else {
+        //convert the currentOperation to mathematical operation then store the result
+        result = new Function(`return ${currentOperation}`)();
+        //check if result is decimal and has repeating decimals (+5 decimal places), then set decimal limiation to 4
+        if (result.toString().includes(".")) {
+          result = decimalPlaces(result) < 5 ? result : result.toFixed(4);
+        }
+        //to continue the opertaion, we set result to currentOperation
+        currentOperation = result;
+        displayPreviousOperation(currentOperation);
+        displayResult(result);
+      }
     } else if (btnValue === "clear") {
       //remove the last item of currentOperation
       currentOperation = currentOperation.slice(0, -1);
-      displayResult(currentOperation);
+      //check if currentOperation is empty, show 0 as result
+      currentOperation.length === 0
+        ? displayResult(0)
+        : displayResult(currentOperation);
     } else if (btnValue === "clear-all") {
       //reset currentOperation and result
       currentOperation = "";
@@ -47,24 +60,29 @@ document
       displayPreviousOperation(0);
       displayResult(0);
     } else if (btnValue === ".") {
+      lastOperand = getLastOperand(currentOperation);
       //check if last operand has . and prevent from adding it more than once
-      if (!getLastOperand(currentOperation).includes(".")) {
+      if (!lastOperand.includes(".")) {
         currentOperation += btnValue.toString();
       }
       displayResult(currentOperation);
     } else if (btnValue === "negative") {
+      lastOperand = getLastOperand(currentOperation);
       //replace the last operand with converted negative operand
-      currentOperation = currentOperation.replace(
-        `${getLastOperand(currentOperation)}`,
-        `(-${getLastOperand(currentOperation)})`
-      );
+      currentOperation =
+        currentOperation.substring(
+          0,
+          currentOperation.lastIndexOf(lastOperand)
+        ) + `(-${lastOperand})`;
       displayResult(currentOperation);
     } else if (btnValue === "%") {
+      lastOperand = getLastOperand(currentOperation);
       //replace the last operand with operand*0.01
-      currentOperation = currentOperation.replace(
-        `${getLastOperand(currentOperation)}`,
-        `${getLastOperand(currentOperation) * 0.01}`
-      );
+      currentOperation =
+        currentOperation.substring(
+          0,
+          currentOperation.lastIndexOf(lastOperand)
+        ) + `${lastOperand * 0.01}`;
       displayResult(currentOperation);
     }
   });
@@ -84,4 +102,18 @@ function getLastOperand(operation) {
   const operandsArray = operation.split(/[^0-9.%]+/);
   //return the last operand
   return operandsArray[operandsArray.length - 1];
+}
+
+function decimalPlaces(num) {
+  //separate integer and decimal parts by .
+  const numDigits = num.toString().split(".");
+  //return number of decimal places
+  return numDigits.length > 0 ? numDigits[1].length : 0;
+}
+
+function checkZeroDivision(operation) {
+  //regex = any number that is followed by /0
+  const regex = /\d+\/0/;
+  //check if there is any number/0 in operation
+  return regex.test(operation);
 }
