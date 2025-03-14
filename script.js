f3fc370f2359dad7b9c2779a5f2fe0a885e9c373
Sarra-Ramezani <160700585+Sarra-Ramezani@users.noreply.github.com>
2025-03-14 23:17:10 +0330

    add keyboard event and refactor code by declaring functions

--- script.js
+++ script.js
@@ -3,10 +3,12 @@
 let btnValue = 0; //stores target button value
 let currentOperation = ""; //stores the whole operation step by step
 let result = 0; // stores the result of current operation
-let lastOperand;
+let lastOperand; //stores the last operand if needed
+let operationsList = []; //stores previous operations
 const history = document.querySelector("#history");
 const calculator = document.querySelector(".calculator");
 const historyButton = document.querySelector("#history-btn");
+const previousOperationsList = document.querySelector("#previous-operations");
 
 historyButton.addEventListener("click", () => {
   if (history.classList.contains("show")) {
@@ -48,66 +50,134 @@ document
       currentOperation += btnValue.toString();
       displayResult(currentOperation);
     } else if (btnValue === "equals") {
-      //check if there is any division by zero
-      if (checkZeroDivision(currentOperation))
-        alert("You can't divide by zero! Try another operation.");
-      else {
-        //convert the currentOperation to mathematical operation then store the result
-        result = new Function(`return ${currentOperation}`)();
-        //check if result is decimal and has repeating decimals (+5 decimal places), then set decimal limiation to 4
-        if (result.toString().includes(".")) {
-          result = decimalPlaces(result) < 5 ? result : result.toFixed(4);
-        }
-        //to continue the opertaion, we set result to currentOperation
-        currentOperation = Number(result);
-        displayPreviousOperation(currentOperation);
-        displayResult(result);
-      }
+      currentOperation = calculate(currentOperation);
     } else if (btnValue === "clear") {
-      //remove the last item of currentOperation
-      currentOperation = currentOperation.slice(0, -1);
-      //check if currentOperation is empty, show 0 as result
-      currentOperation.length === 0
-        ? displayResult(0)
-        : displayResult(currentOperation);
+      currentOperation = clear(currentOperation);
     } else if (btnValue === "clear-all") {
-      //reset currentOperation and result
-      currentOperation = "";
-      result = "";
-      displayPreviousOperation(0);
-      displayResult(0);
+      result = currentOperation = clearAll(currentOperation);
     } else if (btnValue === ".") {
-      lastOperand = getLastOperand(currentOperation);
-      //check if last operand has . and prevent from adding it more than once
-      if (!lastOperand.includes(".")) {
-        currentOperation += btnValue.toString();
-      }
-      displayResult(currentOperation);
+      currentOperation = addDecimalPoint(currentOperation);
     } else if (btnValue === "negative") {
-      lastOperand = getLastOperand(currentOperation);
-      //replace the last operand with converted negative operand
-      console.log(lastOperand);
-      currentOperation =
-        currentOperation.substring(
-          0,
-          currentOperation.lastIndexOf(lastOperand)
-        ) + `(-${lastOperand})`;
-      displayResult(currentOperation);
+      currentOperation = convertToNegative(currentOperation);
     } else if (btnValue === "%") {
-      lastOperand = getLastOperand(currentOperation);
-      //replace the last operand with operand*0.01
-      currentOperation =
-        currentOperation.substring(
-          0,
-          currentOperation.lastIndexOf(lastOperand)
-        ) + `${lastOperand * 0.01}`;
-      displayResult(currentOperation);
+      currentOperation = percentageCalculator(currentOperation);
     }
   });
 
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  console.log(key);
+  //check if the target button is operand or basic operators
+  if (/[0-9+-/*]/.test(key)) {
+    //add target button's value to currentOperation until clicking the equals (=)
+    currentOperation += key.toString();
+    displayResult(currentOperation);
+  } else if (key === "=" || key === "Enter")
+    currentOperation = calculate(currentOperation);
+  else if (key === "c" || key === "Backspace")
+    currentOperation = clear(currentOperation);
+  else if (key === ".") currentOperation = addDecimalPoint(currentOperation);
+  else if (key === "n") currentOperation = convertToNegative(currentOperation);
+  else if (key === "%")
+    currentOperation = percentageCalculator(currentOperation);
+  else if (key === "Escape") currentOperation = clearAll(currentOperation);
+});
+
+function calculate(currentOperation) {
+  //check if there is any division by zero
+  if (checkZeroDivision(currentOperation))
+    alert("You can't divide by zero! Try another operation.");
+  else {
+    //convert the currentOperation to mathematical operation then store the result
+    result = new Function(`return ${currentOperation}`)();
+    //check if result is decimal and has repeating decimals (+5 decimal places), then set decimal limiation to 4
+    if (result.toString().includes(".")) {
+      result = decimalPlaces(result) < 5 ? result : result.toFixed(4);
+    }
+    //add current operation to list
+    currentOperation += `=${result}`;
+    createOperationList(currentOperation);
+
+    //to continue the opertaion, we set result to currentOperation
+    currentOperation = Number(result);
+    displayPreviousOperation(currentOperation);
+    displayResult(result);
+    return currentOperation;
+  }
+}
+
+function clear(currentOperation) {
+  //remove the last item of currentOperation
+  currentOperation = currentOperation.slice(0, -1);
+  //check if currentOperation is empty, show 0 as result
+  currentOperation.length === 0
+    ? displayResult(0)
+    : displayResult(currentOperation);
+  return currentOperation;
+}
+
+function clearAll(currentOperation) {
+  //reset currentOperation and result
+  currentOperation = result = "";
+  displayPreviousOperation(0);
+  displayResult(0);
+  return currentOperation;
+}
 //display passed argument in the #result
 function displayResult(value) {
   document.querySelector("#result").textContent = value;
+}
+
+function addDecimalPoint(currentOperation) {
+  lastOperand = getLastOperand(currentOperation);
+  //check if last operand has . and prevent from adding it more than once
+  if (!lastOperand.includes(".")) {
+    currentOperation += ".";
+  }
+  displayResult(currentOperation);
+  return currentOperation;
+}
+
+function convertToNegative(currentOperation) {
+  lastOperand = getLastOperand(currentOperation);
+  //replace the last operand with converted negative operand
+  console.log(lastOperand);
+  const regex = /[0-9.%]+/;
+  //check if it's an operation or it's just a number
+  if (regex.test(currentOperation) || currentOperation[0] === "-") {
+    //if it's just a number, convert it to negative
+    currentOperation = `(-${currentOperation})`;
+  } else {
+    //if it's an operation, replace last operand with negative one
+    currentOperation =
+      currentOperation.substring(0, currentOperation.lastIndexOf(lastOperand)) +
+      `(-${lastOperand})`;
+  }
+  displayResult(currentOperation);
+  return currentOperation;
+}
+
+function percentageCalculator(currentOperation) {
+  lastOperand = getLastOperand(currentOperation);
+  //replace the last operand with operand*0.01
+  // currentOperation =
+  //   currentOperation.substring(
+  //     0,
+  //     currentOperation.lastIndexOf(lastOperand)
+  //   ) + `${lastOperand * 0.01}`;
+  const regex = /[^0-9.%]+/;
+  //check if it's an operation or it's just a number
+  if (regex.test(currentOperation) || currentOperation[0] === "-") {
+    //if it's just a number, convert it to negative
+    currentOperation = `${currentOperation * 0.01}`;
+  } else {
+    //if it's an operation, replace last operand with negative one
+    currentOperation =
+      currentOperation.substring(0, currentOperation.lastIndexOf(lastOperand)) +
+      `${lastOperand * 0.01}`;
+  }
+  displayResult(currentOperation);
+  return currentOperation;
 }
 
 //display passed argument in the #operation
@@ -139,4 +209,12 @@ function checkZeroDivision(operation) {
   const regex = /\d+\/0/;
   //check if there is any number/0 in operation
   return regex.test(operation);
+}
+
+//get the whole operation each time and store it in a list and display the list
+function createOperationList(operation) {
+  operationsList.push(operation);
+  operationsList.forEach((element) => {
+    previousOperationsList.innerHTML += `${element} <br>`;
+  });
 }
